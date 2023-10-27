5794e12453734094f2f8016873691c5ac5fabfd1
VitaliiOsadchukIF <radistje@gmail.com>
2023-10-27 23:52:11 +0300

    create Time Servlet

---
+++ src/main/java/org/example/TimeServlet.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.io.IOException;
+import java.time.DateTimeException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class TimeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Отримуємо параметр timezone з запиту. Якщо він не переданий, то використовуємо UTC
+        String timezone = req.getParameter("timezone");
+        if (timezone == null) {
+            timezone = "UTC";
+        }
+
+        try {
+            // Отримуємо поточний час в заданому часовому поясі
+            ZonedDateTime zdt = ZonedDateTime.now(ZoneId.of(timezone));
+
+            // Форматуємо час до рядка
+            String time = zdt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z"));
+
+            // Відправляємо HTML сторінку з часом
+            resp.getWriter().println("<html><body><h1>" + time + "</h1></body></html>");
+        } catch (DateTimeException e) {
+            // Якщо переданий часовий пояс недійсний, повертаємо помилку
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().println("<html><body><h1>Invalid timezone</h1></body></html>");
+        }
+
+    }
+}
